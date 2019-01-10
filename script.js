c97e5f8814d573e1921a6228a143974f2c885bb4
JuniperMakesStuff <45408795+JuniperMakesStuff@users.noreply.github.com>
2019-01-10 15:48:12 -0800

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ window.onload = function() {
     var canvas = document.getElementById("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    canvas.requestFullscreen();
+    document.requestFullscreen();
     var ctx = canvas.getContext("2d");
 
     src.connect(analyser);
